a883162b76dd075412ea5984c0c82d300980bdc3
Shraddha Gade <gadeshraddha@yahoo.com>
2026-06-14 20:03:08 +0530

    Largest Number of three

---
+++ MaxOfThree.py
@@ -0,0 +1,14 @@
+def find_max(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+ 
+ 
+x = float(input("Enter first number: "))
+y = float(input("Enter second number: "))
+z = float(input("Enter third number: "))
+print(f"The largest number is {find_max(x, y, z)}")
+ 
