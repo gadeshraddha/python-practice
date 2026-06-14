77095fee49060868337855b88764e2a53aecb4e6
Shraddha Gade <gadeshraddha@yahoo.com>
2026-06-14 20:06:15 +0530

    Calculate area of a reactangle

---
+++ RectArea.py
@@ -0,0 +1,8 @@
+def rectangle_area(length, width):
+    return length * width
+ 
+ 
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+print(f"Area = {rectangle_area(length, width)}")
+ 
