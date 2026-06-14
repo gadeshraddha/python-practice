967219b469e677b693f37271c4be5c189f27e43c
Shraddha Gade <gadeshraddha@yahoo.com>
2026-06-14 20:18:35 +0530

    guessing if the number is even or odd

---
+++ EvenOddGame.py
@@ -0,0 +1,9 @@
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+ 
+ 
+number = int(input("Enter a number: "))
+print(f"{number} is {check_even_odd(number)}")
